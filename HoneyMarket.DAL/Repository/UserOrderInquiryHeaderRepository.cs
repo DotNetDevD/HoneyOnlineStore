8a65c36c6739d489bf16290546b8c876b5875587
WtfMontag <fahrenheit843f@gmail.com>
2023-01-29 18:38:39 +0300

    add Logic with User order in cart controller

---
+++ HoneyMarket.DAL/Repository/UserOrderInquiryHeaderRepository.cs
@@ -0,0 +1,20 @@
+﻿using HoneyMarket.DAL.Repository.IRepository;
+using HoneyMarket.Models;
+using HoneyOnlineStore.DAL;
+
+namespace HoneyMarket.DAL.Repository
+{
+    public class UserOrderInquiryHeaderRepository : Repository<UserOrderInquiryHeader>, IUserOrderInquiryHeaderRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public UserOrderInquiryHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(UserOrderInquiryHeader userOrderInquiryHeader)
+        {
+            _db.UserOrderInquiryHeader.Update(userOrderInquiryHeader);
+        }
+    }
+}
